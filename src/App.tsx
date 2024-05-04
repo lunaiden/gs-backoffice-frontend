86e6df342f2138d12f68b0e3b7958e96b9ab3734
lunaiden <melanie.leeman@protonmail.com>
2024-05-04 16:50:42 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div className="container">
+            <h1>Bienvenue sur GoodSpeakVR !</h1>
+        </div>
+    </>
+  )
+}
+
+export default App
