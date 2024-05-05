840c2d02843b17003cc69df7ffae256eb86bcca2
lunaiden <melanie.leeman@protonmail.com>
2024-05-05 17:16:24 +0200

    test

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { HashRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {AuthProvider} from "./hooks/useAuth.tsx";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <AuthProvider>
                 <App/>
             </AuthProvider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>,
 )
